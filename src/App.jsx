f6982ef8742ebf3bca0015e7a14b077d53a6f1b0
Stefan Dunikovskyi <stefan090304@gmail.com>
2022-09-12 18:34:00 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import { GlobalStyle } from "./utils/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./utils/theme.jsx";
-import Layout from "./components/Layout/Layout";
 import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
 import Home from "./components/Home/Home";
 import Movies from "./components/Movies/Movies";
 import CardInfo from "./components/Home/CardInfo/CardInfo";
 import CardCast from "./components/Home/CardInfo/CardCast";
 import CardReviews from "./components/Home/CardInfo/CardReviews";
+//import { lazy, Suspense } from "react";
+//import { Loader } from "./components/Loader/Loader";
+// const Layout = lazy(() => import("./components/Home/Home"));
+// const Home = lazy(() => import("./components/Layout/Layout"));
+// const Movies = lazy(() => import("./components/Movies/Movies"));
+// const CardInfo = lazy(() => import("./components/Home/CardInfo/CardInfo"));
+// const CardCast = lazy(() => import("./components/Home/CardInfo/CardCast"));
+// const CardReviews = lazy(() =>
+//   import("./components/Home/CardInfo/CardReviews")
+// );
 export const App = () => {
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
+        {/* <Suspense fallback={<Loader></Loader>}> */}
         <Routes>
           <Route path="/" element={<Layout></Layout>}>
             <Route index element={<Home></Home>} />
@@ -24,6 +35,7 @@ export const App = () => {
             <Route path="*" element={<Navigate to="/"></Navigate>}></Route>
           </Route>
         </Routes>
+        {/* </Suspense> */}
       </ThemeProvider>
     </>
   );
